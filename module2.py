496a6ed217185c6687a4b2730156e6e425579bb5
Анастасия Миронова <nastymironkiss@gmail.com>
2022-11-17 14:12:57 +0700

    blank.py graphic_arts main.py module1.py module2.py temp.py

---
+++ module2.py
@@ -0,0 +1,8 @@
+# Из модуля module1.py импортируем функцию special_calculation.
+from module1 import special_calculation
+
+
+print("Основной модуль")
+# Производим расчёты.
+coef = special_calculation(2, 5)
+print(coef)
